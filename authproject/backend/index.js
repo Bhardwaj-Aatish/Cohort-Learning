a7e841ab8e268821de44e1415eeea62b81ad5710
Bhardwaj <aatbhard@ciena.com>
2025-01-15 14:41:58 +0530

    Use json web tokens in index.js file and removed  generateToken function

--- authproject/backend/index.js
+++ authproject/backend/index.js
@@ -1,20 +1,11 @@
 const express = require('express');
+const jwt = require('jsonwebtoken')
 const app = express();
 
 const users = [];
+const JWT_SECRET = 'randomkeyajdljalfdj234js'
 
 app.use(express.json())
-
-const generateToken = () => {
-
-  let token = '';
-  const options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-  for(let i=0; i < 32; i++) {
-    token += options[Math.floor(Math.random() * options.length)];
-  }
-  return token
-}
 
 app.post('/signup', (req, res) => {
   const username = req.body.username;
@@ -45,9 +36,10 @@ app.post('/signin', (req, res) => {
 
   const user = users.find(user => user.username === username && user.password === password);
 
-  if(user) {
-    const token = generateToken()
-    user.token = token
+  if (user) {
+    const token = jwt.sign({
+      username
+    }, JWT_SECRET)
     res.send({
       token
     })
@@ -61,9 +53,10 @@ app.post('/signin', (req, res) => {
 
 app.get('/me', (req, res) => {
   const token = req.headers.token;
-  const user = users.find(user => user.token === token);
-
-  if(user) {
+  const currentUser = jwt.verify(token, JWT_SECRET)
+  
+  if(currentUser.username) {
+    const user = users.find(user => user.username === currentUser.username);
     res.send({
       username: user.username,
       password: user.password
