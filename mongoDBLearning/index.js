8e4d3c814130255756ca48016590adca2c163d00
unknown <aatishbhardwaj03@gmail.com>
2025-01-23 23:58:48 +0530

    implemented todos backend without database

--- mongoDBLearning/index.js
+++ mongoDBLearning/index.js
@@ -1,19 +1,103 @@
-const express = require('express')
-const mongoose = require('mongoose')
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require('jsonwebtoken')
 
-mongoose
-  .connect('mongodb+srv://aatishmongodb:Sourav1stCiena@cluster0.kcfiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
-  .then(() => {
-    console.log('Connected to MongoDB successfully');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
+const JWT_SECRET = 'randomjlajdfoaejlsfa'
+
+const users = []
+const todos = []
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Your app is working fine')
+function auth (req, res, next) {
+  const token = req.headers.token;
+
+  const currentUserEmail = jwt.verify(token, JWT_SECRET)
+
+  if(currentUserEmail) {
+    req.email = currentUserEmail;
+    next();
+  } else {
+    res.status(403).send({
+      message: 'You are not allowed'
+    })
+  }
+
+}
+
+
+app.post('/signup', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  const user = users.find(user => user.email === email)
+
+  if(user) {
+    res.status(403).send({
+      message: 'Email is not unique'
+    })
+  } else {
+    users.push({
+      name,
+      email,
+      password
+    })
+    res.send({
+      message: 'You have signed up successfully'
+    })
+  }
 })
 
-app.listen(3000);
+app.post('/signin', (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+
+  const user = users.find(user => user.email === email && user.password === password)
+
+  if(user) {
+    const token = jwt.sign({
+      email
+    }, JWT_SECRET);
+
+    res.send({
+      token
+    })
+  } else {
+    res.status(403).send({
+      message: 'Credentials are incorrect'
+    })
+  }
+})
+
+app.post('/todo', auth,  (req, res) => {
+  const title = req.body.title;
+  const done = req.body.title;
+
+  todos.push({
+    title,
+    done
+  })
+
+  res.send({
+    message: 'Todos sent successfully'
+  })
+
+})
+
+app.get('/todos', auth, (req, res) => {
+  res.send({
+    todos
+  })
+
+})
+
+
+async function startServer() {
+  // await mongoose.connect("mongodb+srv://aatishmongodb:Sourav1stCiena@cluster0.kcfiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/mongodbTest").then(() => console.log("your mongodb is connected now"))
+  app.listen(3000);
+}
+
+startServer()
